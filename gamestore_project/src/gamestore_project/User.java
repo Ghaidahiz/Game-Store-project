0ee3506cb2a4b03b104b1b96e547dfd32180f7e4
Ghaida <152740404+Ghaidahiz@users.noreply.github.com>
2024-02-29 03:37:49 +0300

    wrote methods headings and attributes in User.java

--- gamestore_project/src/gamestore_project/User.java
+++ gamestore_project/src/gamestore_project/User.java
@@ -1,25 +1,28 @@
 package gamestore_project;
 
 public class User {
-private String username;
-private int nOfg; //number of games
-private double wallet;
-private Game[] gameLibrary;
+	private String username;
+	private int nOfg; // number of games
+	private double wallet;
+	private Game[] gameLibrary;
 
+	public User(String n, int no, double wallet) {
+	}
 
-public User(String n, int no, double wallet){}
-public boolean buyGame(Game g) {}
+	public boolean buyGame(Game g) {
+	}
 
-public boolean sendGift(String username , Game g) {}
+	public boolean sendGift(String username, Game g) {
+	}
 
-public boolean uninstallGame(String name) {}
+	public boolean uninstallGame(String name) {
+	}
 
-public Game findGame(String name) {}
+	public Game findGame(String name) {
+	}
 
-public String getUsername() {
-	return username;
-}
-
-
+	public String getUsername() {
+		return username;
+	}
 
 }
