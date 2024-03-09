55c0ad876f1ac9fe1276f04919384e6ccfc13e65
ghaida <ghaida@localhost>
2024-03-10 01:35:18 +0300

    finished some methods and started others

--- gamestore_project/src/gamestore_project/User.java
+++ gamestore_project/src/gamestore_project/User.java
@@ -7,14 +7,63 @@ private double wallet;
 private Game[] gameLibrary;
 
 
-public User(String n, int no, double wallet){}
-public boolean buyGame(Game g) {}
+public User(String n,int size, double wallet){
+	username =n;
+	nOfg=0;
+	this.wallet=wallet;
+	gameLibrary= new Game[size];//idk about dat
+	
+}
 
-public boolean sendGift(String username , Game g) {}
+public boolean buyGame(Game g) { //*** I may need too cast g and make cases for each child of games but this is the basic idea***
+	if (g.priceAfterDiscount() <= wallet) {//also I need to check if game exists in library**
+		gameLibrary[nOfg++]=g;
+		wallet = wallet - g.priceAfterDiscount();
+		return true;
+	}
 
-public boolean uninstallGame(String name) {}
+	else return false;
+	
+}
 
-public Game findGame(String name) {}
+
+
+public boolean sendGift(User friend , Game g) {
+	if (g.priceAfterDiscount() <= wallet ) {
+		friend.gameLibrary[friend.nOfg++]=g;
+		this.wallet = this.wallet - g.priceAfterDiscount();
+		return true;
+	}
+	else return false;
+	
+
+}
+
+public boolean uninstallGame(String name) {
+	for(int i = 0; i<nOfg;i++) {
+		if(gameLibrary[i].name.equalsIgnoreCase(name)) {
+			gameLibrary[i]=gameLibrary[nOfg-1];
+			gameLibrary[--nOfg]=null;
+			return true;
+		}
+		
+	}
+	return false;
+	
+}
+
+
+
+public Game findGame(String name) {
+	for(int i=0;i<nOfg;i++) {
+		if(gameLibrary[i].name.equalsIgnoreCase(name)) {
+			return gameLibrary[i];
+		}
+
+	}
+	return null; //if game is not found
+	
+}
 
 public String getUsername() {
 	return username;
