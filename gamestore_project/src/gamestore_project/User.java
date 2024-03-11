616ccfaedbc33611f3e71b251f1605c02ee2d65c
Ghaidahiz <ghz_11@hotmail.com>
2024-03-11 17:56:12 +0300

    modified user methods and added prints

--- gamestore_project/src/gamestore_project/User.java
+++ gamestore_project/src/gamestore_project/User.java
@@ -2,65 +2,69 @@ package gamestore_project;
 
 public class User {
 private String username;
-private int nOfg; //number of games
+private int numberOfGames; 
 private double wallet;
 private Game[] gameLibrary = new Game[50];
 
 
 public User(String n, double wallet){
 	username =n;
-	nOfg=0;
+	numberOfGames=0;
 	this.wallet=wallet;
 	
 }
 
-public boolean buyGame(Game g) {
-	if(g==null)
-		return false;
+public void buyGame(Game g) {
+	if(g==null){
+		System.out.println("the game that you want to purchase doesn't exist in this gamestore");}
 	if (g.priceAfterDiscount() <= wallet) {
-		gameLibrary[nOfg++]=g;
-		wallet = wallet - g.priceAfterDiscount();
-		return true;
-	}
-
-	else return false;
+		gameLibrary[numberOfGames++]=g;
+		wallet = wallet - g.priceAfterDiscount();}
+	else
+	{ 
+		System.out.println("You don't have enough funds in your wallet, can't complete purchase");}
 	
 }
 
 
 
-public boolean sendGift(User friend , Game g) {
+public void sendGift(User friend , Game g) {
 	if (g==null || friend==null)
-		return false;
-	
-	if (g.priceAfterDiscount() <= wallet && friend.nOfg < friend.gameLibrary.length) {
-		friend.gameLibrary[friend.nOfg++]=g;
-		this.wallet = this.wallet - g.priceAfterDiscount();
-		return true;
+	return;
+	 if (friend.numberOfGames < friend.gameLibrary.length) {
+		if(g.priceAfterDiscount() <= wallet ){
+		friend.gameLibrary[friend.numberOfGames++]=g;
+		this.wallet = this.wallet - g.priceAfterDiscount();	}
+			else{
+				System.out.println("You don't have enough funds in your wallet, can't complete purchase");
+			}
 	}
-	else return false;
-	
+		else{
+			System.out.println("the recipient's library is full");
 
+		}
 }
 
-public boolean uninstallGame(String name) {
-	for(int i = 0; i<nOfg;i++) {
+public void uninstallGame(String name) {
+	for(int i = 0; i<numberOfGames;i++) {
 		if(gameLibrary[i].getName().equalsIgnoreCase(name)) {
-			gameLibrary[i]=gameLibrary[nOfg-1];
-			gameLibrary[--nOfg]=null;
-			return true;
+			gameLibrary[i]=gameLibrary[numberOfGames-1];
+			gameLibrary[--numberOfGames]=null;
+			System.out.println( name + " was removed successfully");
+			return;
 		}
 		
 	}
-	return false;
+	System.out.println(name + "doesn't exist in your llibrary");
+	return;
 	
 }
 
 
 
 public Game findGame(String name) {
-	for(int i=0;i<nOfg;i++) {
-		if(gameLibrary[i].name.equalsIgnoreCase(name)) {
+	for(int i=0;i<numberOfGames;i++) {
+		if(gameLibrary[i].getName().equalsIgnoreCase(name)) {
 			return gameLibrary[i];
 		}
 
