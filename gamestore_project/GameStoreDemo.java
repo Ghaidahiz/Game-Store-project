d32c2d4a7ab43a0e6a13d60277c0bfb933479c88
AlmahaDevv <161609153+AlmahaDevv@users.noreply.github.com>
2024-03-11 17:36:09 +0000

    main update

--- gamestore_project/GameStoreDemo.java
+++ gamestore_project/GameStoreDemo.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class GameStoreDemo {
     public static void main(String[] args) {
+<<<<<<< HEAD
         Scanner sc = new Scanner(System.in);
 
         Admin Almaha = new Admin("Almaha", "123");
@@ -88,6 +89,157 @@ public class GameStoreDemo {
                 System.out.println("invalid input , try again..");
 
         } while (check != 1 && check != 2);
+=======
+       Scanner sc = new Scanner(System.in); 
+
+
+       GameStore GEA = new GameStore("GEA");
+
+System.out.println("GEA GAME STORE");
+int check;
+
+do
+{
+System.out.println("ARE YOU A .. \n 1- USER? \n 2-ADMIN?");
+ check = sc.nextInt();
+
+if(check==1)
+{
+    
+    System.out.println("PLEASE ENTER YOUR NAME :");
+   String name = sc.next();
+   if(GEA.findUser(name)==null)
+   {
+    System.out.println("WELCOME TO OUR STORE , PLEASE FILL THE NEXT INFORMATION");
+    System.out.println("PLEASE ENTER YOUR WALLET AMOUNT :");
+ double wallet = sc.nextDouble();
+
+  User u1 = new User(name, wallet );
+   }
+else
+System.out.println("WELCOME BACK !");
+
+
+System.out.println(" PLEASE SELECT NUMBER OF SERVICE THAT YOU WANT : ");
+System.out.println("1- BUY A GAME ? \n 2-SEND GIFT TO A FRIEND ? \n 3-UNINSTALL GAME ? \n 4- FIND GAME ?");
+int choice = sc.nextInt();
+
+while (choice<0 && choice>4 ) {
+    System.out.println("WRONG INPUT , PLEASE TRY AGAIN ?");
+    System.out.println("1- BUY A GAME ? \n 2-SEND GIFT TO A FRIEND ? \n 3-UNINSTALL GAME ? \n 4- FIND GAME ? \n 5-SHOW ALL GAMES IN LIBRARY ? \n 0-EXIT ?");
+    choice = sc.nextInt(); 
+}
+
+switch (choice) {
+    case 1:
+    System.out.println("ENTER THE NAME OF THE GAME ..");
+    String Gname = sc.nextLine();
+    if(u1.buyGame(GEA.searchForGame(Gname)))
+System.out.println("SUCCESFULLY ADDED ");
+else
+System.out.println("GAME NOT FOUND");     
+        break;
+
+    case 2:
+System.out.println(" ENTER THE NAME OF YOUR FRIEND ACCOUNT AND THE NAME OF THE WANTED GAME");
+String Fname = sc.nextLine();
+ Gname = sc.nextLine();
+u1.sendGift(GEA.findUser(Fname), GEA.searchForGame(Gname));
+
+        break;
+
+    case 3:
+    System.out.println("ENTER THE NAME OF THE GAME YOU WANT TO UNINSTALL..");
+     Gname = sc.nextLine();
+u1.uninstallGame(Gname);
+    break;
+
+    case 4:
+    System.out.println("ENTER THE NAME OF THE GAME YOU WANT TO FIND IN YOUR LIBRARY ..");
+    Gname = sc.nextLine();
+u1.findGame(Gname);
+    break;
+
+    case 5 :
+    u1.toString();
+    break;
+
+    case 0:
+    System.exit(0);
+    break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+else
+if(check==2)
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+else
+System.out.println("invalid input , try again..");
+
+
+}while(check!=1 && check!=2);
+
+
+>>>>>>> 97a15e9 (main update)
 
     }
 
